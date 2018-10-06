95159ffbf1e31276ddd6abcad67028e5c13cd7f9
Fiodar Gorodkovets <f.gorodkovets@itransition.com>
2018-10-06 19:01:25 +0300

    some updates

--- Images.py
+++ Images.py
@@ -6,42 +6,63 @@ import png
 
 
 class Images:
+    width = 0
+    height = 0
+    pixels = []
+    metadata = []
+    pixel_byte_width = 0
+    full_file_name = ''
+
     def __init__(self):
-        c = 3
+        self.clear()
 
-    def create_image(self):
-        width = 255
-        height = 255
+    def clear(self):
+        self.width = 0
+        self.height = 0
+        self.pixels = []
+        self.metadata = []
+        self.pixel_byte_width = 0
+        self.full_file_name = ''
 
-        pixel_byte_width = 3
-        pixels = self.create_empty_canvas(width, height, pixel_byte_width)
-
-        pixels = self.process_pixels(pixels, width, height, pixel_byte_width)
-
-        self.save_pixels(pixels, width, height, pixel_byte_width)
+    # def create_image(self):
+    #     width = 255
+    #     height = 255
+    #
+    #     pixel_byte_width = 3
+    #     pixels = self.create_empty_canvas(width, height, pixel_byte_width)
+    #
+    #     pixels = self.process_pixels(pixels, width, height, pixel_byte_width)
+    #
+    #     self.save_pixels(pixels, width, height, pixel_byte_width)
 
     def load_image(self, filename):
+        self.clear()
+
         reader = png.Reader(filename=filename)
-        width, height, raw_pixels, metadata = reader.read()
-        full_file_name = basename(reader.file.name)
+        self.width, self.height, raw_pixels, self.metadata = reader.read()
+        self.full_file_name = basename(reader.file.name)
 
-        pixel_byte_width = 4 if metadata['alpha'] else 3
-        pixels = self.convert_to_lists(raw_pixels, pixel_byte_width)
+        self.pixel_byte_width = 4 if self.metadata['alpha'] else 3
+        self.pixels = self.convert_to_lists(raw_pixels)
 
-        pixels = self.process_pixels(pixels, width, height, pixel_byte_width)
-
-        self.save_image(pixels, width, height, metadata, full_file_name)
-
-    def process_pixels(self, pixels, width, height, pixel_byte_width):
-        for i in range(0, width):
-            for j in range(0, height):
-                color = pixels[i][j]
+    def convert_to_gray(self):
+        for i in range(0, self.width):
+            for j in range(0, self.height):
+                color = self.pixels[i][j]
                 gray = int(round(color[0] * 0.3 + color[1] * 0.59 + color[2] * 0.11))
-                if gray != 0:
-                    c = gray
-                pixels[i][j] = [gray, gray, gray, color[3]] if pixel_byte_width == 4 else [gray, gray, gray]
+                self.pixels[i][j] = [gray, gray, gray, color[3]] if self.pixel_byte_width == 4 else [gray, gray, gray]
 
-        return pixels
+        self.full_file_name = 'gray_' + self.full_file_name
+
+    def get_red(self):
+        red = [0] * 256
+        for i in range(0, self.width):
+            for j in range(0, self.height):
+                color = self.pixels[i][j]
+                red[color[0]] += 1
+
+        del red[0]
+        return red
 
     def save_pixels(self, pixels, width, height, pixel_byte_width, full_file_name):
         raw_pixels = self.convert_to_array(pixels, pixel_byte_width)
@@ -50,13 +71,13 @@ class Images:
         writer.write_array(output, raw_pixels)
         output.close()
 
-    def save_image(self, pixels, width, height, metadata, full_file_name):
-        pixel_byte_width = 4 if metadata['alpha'] else 3
-        raw_pixels = self.convert_to_array(pixels, pixel_byte_width)
-        filename, extension = splitext(full_file_name)
+    def save_image(self):
+        pixel_byte_width = 4 if self.metadata['alpha'] else 3
+        raw_pixels = self.convert_to_array(self.pixels, pixel_byte_width)
+        filename, extension = splitext(self.full_file_name)
 
         output = open(filename + '_line' + extension, 'wb')
-        writer = png.Writer(width, height, **metadata)
+        writer = png.Writer(self.width, self.height, **self.metadata)
         writer.write_array(output, raw_pixels)
         output.close()
 
@@ -70,14 +91,14 @@ class Images:
 
         return pixels
 
-    def convert_to_lists(self, raw_pixels, pixel_width):
+    def convert_to_lists(self, raw_pixels):
         index = 0
         pixels = []
         for line in list(raw_pixels):
             pixels.append([])
             line = list(line)
-            for i in range(0, len(line), pixel_width):
-                pixel = line[i:i + pixel_width]
+            for i in range(0, len(line), self.pixel_byte_width):
+                pixel = line[i:i + self.pixel_byte_width]
                 pixels[index].append(pixel)
 
             index += 1
--- __init__.py
+++ __init__.py
@@ -1,5 +1,32 @@
 from Images import Images
+import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     image = Images()
     image.load_image('Images/icon.png')
+    # image.convert_to_gray()
+    red = image.get_red()
+
+    image.save_image()
+
+    x_labels = range(255)
+    data_values = range(255, 0, -1)
+    data_values2 = range(255)
+
+    plt.subplot(311)
+    plt.plot(x_labels, red)
+    plt.title('A tale of 2 subplots')
+    plt.ylabel('Undamped1')
+
+    plt.subplot(312)
+    plt.plot(x_labels, data_values2)
+    plt.xlabel('time (s)')
+    plt.ylabel('Undamped2')
+
+    plt.subplot(313)
+    plt.plot(x_labels, data_values2)
+    plt.xlabel('time (s)')
+    plt.ylabel('Undamped3')
+
+    plt.show()
+
